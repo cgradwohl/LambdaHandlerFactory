c6cf37d00b098471bc8d2020cf52983786571a25
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2023-12-16 20:50:06 -0800

    types.

--- src/factory.ts
+++ src/factory.ts
@@ -1,78 +1,80 @@
-import { APIGatewayProxyHandler, KinesisStreamHandler, Context, APIGatewayEvent, APIGatewayProxyResult, Callback } from 'aws-lambda';
+import { APIGatewayProxyHandler, KinesisStreamHandler, Context, APIGatewayEvent, APIGatewayProxyResult, Callback, KinesisStreamEvent, KinesisStreamBatchResponse, APIGatewayProxyEvent } from 'aws-lambda';
 
 interface ExtendedContext extends Context {
   tenantId: string;
   sessionId: string;
 }
 
-type ExtendedAPIGatewayProxyHandler = (event: APIGatewayEvent, context: ExtendedContext, callback: Callback<APIGatewayProxyResult>) => void | Promise<APIGatewayProxyResult>;
-type ExtendedKinesisStreamHandler = (event: any, context: ExtendedContext, callback: Callback<any>) => void | Promise<any>;
+type APIGatewayHandler = (event: APIGatewayProxyEvent, context: ExtendedContext, callback: Callback<APIGatewayProxyResult>) => void | Promise<APIGatewayProxyResult>;
 
-type MiddlewareFunction<T extends ExtendedAPIGatewayProxyHandler | ExtendedKinesisStreamHandler> = (handler: T) => T;
+type KinesisHandler = (event: KinesisStreamEvent, context: ExtendedContext, callback: Callback<any>) => void | Promise<KinesisStreamBatchResponse>;
 
-function withErrorHandling<T extends ExtendedAPIGatewayProxyHandler | ExtendedKinesisStreamHandler>(handler: T): T {
-  return (async (event: any, context: ExtendedContext, callback: Callback<APIGatewayProxyResult> & Callback<any>) => {
-    try {
-      return await handler(event, context, callback);
-    } catch (error) {
-      console.error("Error in handler:", error);
-      throw error; // Or handle the error as per your logic
-    }
-  }) as T;
-}
+type MiddlewareFunction = (handler: APIGatewayProxyHandler | KinesisStreamHandler) => APIGatewayHandler | KinesisHandler;
 
-function withTenantResolution<T extends ExtendedAPIGatewayProxyHandler | ExtendedKinesisStreamHandler>(handler: T): T {
-  return (async (event: { headers: { [x: string]: any; }; }, context: any, callback: Callback<APIGatewayProxyResult> & Callback<any>) => {
-    if (!event.headers || !event.headers['X-Tenant-ID']) {
-      throw new Error("Missing Tenant ID");
-    }
-    const extendedContext = { ...context, tenantId: event.headers['X-Tenant-ID'] };
-    return handler(event, extendedContext as ExtendedContext, callback);
-  }) as T;
-}
+// function withErrorHandling<T extends ExtendedAPIGatewayProxyHandler | ExtendedKinesisStreamHandler>(handler: T): T {
+//   return (async (event: any, context: ExtendedContext, callback: Callback<APIGatewayProxyResult> & Callback<any>) => {
+//     try {
+//       return await handler(event, context, callback);
+//     } catch (error) {
+//       console.error("Error in handler:", error);
+//       throw error; // Or handle the error as per your logic
+//     }
+//   }) as T;
+// }
 
-function withAuthorization<T extends ExtendedAPIGatewayProxyHandler | ExtendedKinesisStreamHandler>(handler: T): T {
-  return (async (event: { headers: { [x: string]: any; }; }, context: any, callback: Callback<APIGatewayProxyResult> & Callback<any>) => {
-    if (!event.headers || !event.headers['Authorization']) {
-      throw new Error("Missing Authorization");
-    }
-    const extendedContext = { ...context, sessionId: 'extracted-session-id' }; // Extract sessionId as needed
-    return handler(event, extendedContext as ExtendedContext, callback);
-  }) as T;
-}
+// function withTenantResolution<T extends ExtendedAPIGatewayProxyHandler | ExtendedKinesisStreamHandler>(handler: T): T {
+//   return (async (event: { headers: { [x: string]: any; }; }, context: any, callback: Callback<APIGatewayProxyResult> & Callback<any>) => {
+//     if (!event.headers || !event.headers['X-Tenant-ID']) {
+//       throw new Error("Missing Tenant ID");
+//     }
+//     const extendedContext = { ...context, tenantId: event.headers['X-Tenant-ID'] };
+//     return handler(event, extendedContext as ExtendedContext, callback);
+//   }) as T;
+// }
 
-function HandlerFactory<T extends ExtendedAPIGatewayProxyHandler | ExtendedKinesisStreamHandler>(handler: T, middlewares: MiddlewareFunction<T>[] = []): T {
-  return middlewares.reduce((currentHandler, middleware) => middleware(currentHandler), handler);
-}
+// function withAuthorization<T extends ExtendedAPIGatewayProxyHandler | ExtendedKinesisStreamHandler>(handler: T): T {
+//   return (async (event: { headers: { [x: string]: any; }; }, context: any, callback: Callback<APIGatewayProxyResult> & Callback<any>) => {
+//     if (!event.headers || !event.headers['Authorization']) {
+//       throw new Error("Missing Authorization");
+//     }
+//     const extendedContext = { ...context, sessionId: 'extracted-session-id' }; // Extract sessionId as needed
+//     return handler(event, extendedContext as ExtendedContext, callback);
+//   }) as T;
+// }
 
-function APIGatewayProxyHandlerFactory(handler: ExtendedAPIGatewayProxyHandler, middlewares: MiddlewareFunction<ExtendedAPIGatewayProxyHandler>[] = []): ExtendedAPIGatewayProxyHandler {
-  const defaultMiddlewares = [withTenantResolution, withAuthorization,];
+// function HandlerFactory<T extends ExtendedAPIGatewayProxyHandler | ExtendedKinesisStreamHandler>(handler: T, middlewares: MiddlewareFunction<T>[] = []): T {
+//   return middlewares.reduce((currentHandler, middleware) => middleware(currentHandler), handler);
+// }
 
-  return HandlerFactory(handler, [...middlewares, ...defaultMiddlewares]);
-}
+// function APIGatewayProxyHandlerFactory(handler: ExtendedAPIGatewayProxyHandler, middlewares: MiddlewareFunction<ExtendedAPIGatewayProxyHandler>[] = []): ExtendedAPIGatewayProxyHandler {
+//   const defaultMiddlewares = [withTenantResolution, withAuthorization,];
 
-function KinesisStreamHandlerFactory(handler: ExtendedKinesisStreamHandler, middlewares: MiddlewareFunction<ExtendedKinesisStreamHandler>[] = []): ExtendedKinesisStreamHandler {
-  const defaultMiddlewares = [withTenantResolution, withAuthorization,];
+//   return HandlerFactory(handler, [...middlewares, ...defaultMiddlewares]);
+// }
 
-  return HandlerFactory(handler, [...middlewares, ...defaultMiddlewares]);
-}
-// -  - - - - - - 
-// worker.ts
-// -  - - - - - - 
-const myHttpHandler: ExtendedAPIGatewayProxyHandler = async (event, context) => {
-  // Your handler logic here
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ tenantId: context.tenantId, sessionId: context.sessionId })
-  };
-};
+// function KinesisStreamHandlerFactory(handler: ExtendedKinesisStreamHandler, middlewares: MiddlewareFunction<ExtendedKinesisStreamHandler>[] = []): ExtendedKinesisStreamHandler {
+//   const defaultMiddlewares = [withTenantResolution, withAuthorization,];
 
-const httpHandler = APIGatewayProxyHandlerFactory(myHttpHandler, [withErrorHandling]);
+//   return HandlerFactory(handler, [...middlewares, ...defaultMiddlewares]);
+// }
+// // -  - - - - - - 
+// // worker.ts
+// // -  - - - - - - 
+// const myHttpHandler: ExtendedAPIGatewayProxyHandler = async (event, context) => {
+//   // Your handler logic here
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({ tenantId: context.tenantId, sessionId: context.sessionId })
+//   };
+// };
+
+// // worker.ts
+// export const handler = APIGatewayProxyHandlerFactory(myHttpHandler, [withErrorHandling]);
 
 
-const myKinesisHandler: ExtendedKinesisStreamHandler = async (event, context) => {
-  // Your handler logic here
-  return {};
-}
+// const myKinesisHandler: ExtendedKinesisStreamHandler = async (event, context) => {
+//   return {};
+// }
 
-const kinesisHandler = KinesisStreamHandlerFactory(myKinesisHandler, [withErrorHandling]);
+// // worker.ts
+// export const handler = KinesisStreamHandlerFactory(myKinesisHandler, [withErrorHandling]);
